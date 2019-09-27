f6fc5771cca4d7a5d2ffeaedd6e6c91e52466a90
Hananou <40833229+Hananou@users.noreply.github.com>
2019-09-28 01:19:53 +0300

    correction of syntax

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_guest_should_see_add_button(browser):
     browser.get(link)
     #time.sleep(30) # <=== раскомментируйте эту строку, чтоб визуально убедиться, что сайт магазина запущен на языке, переданном в параметрах
-    assert browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket') #проверяем наличие кнопки добавления в корзину
+    assert len(browser.find_elements_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket'))> 0, "\n!!!Achtung!!!Attantion!!!Внимание!!! Кнопка не найдена!!" #проверяем наличие кнопки добавления в корзину
 
